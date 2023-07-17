0f99116b90c3fefdca13dee8037d333d49567c2e
devcode99 <ddev61032@gmail.com>
2023-07-17 15:29:07 +0530

    Test the routing with express and mongoose, quick Fix 6

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 async function connectToMongoDB() {
-  try {
+//   try {
     await mongoose.connect('mongodb+srv://ddev61032:RgVYwTu74DeKbbE2@cluster0.snlwgd0.mongodb.net/kung_fu_panda');
     console.log('MongoDB connected successfully');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-  }
+//   } catch (error) {
+//     console.error('MongoDB connection error:', error);
+//   }
 }
 
 module.exports = connectToMongoDB;
