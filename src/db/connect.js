ee1e0f48ac3e3f09156047a390d5ff28cefd63d4
devcode99 <ddev61032@gmail.com>
2023-07-18 15:10:06 +0530

    Mongo client testing

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
+const {MongoClient} = require('mongodb')
 require('dotenv').config();
 
 async function connectToMongoDB() {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}kung_fu_panda`);
-    console.log('MongoDB connected successfully');
+    const client = await MongoClient.connect(process.env.MONGODB_URI)
+    const query = client.db(`kung_fu_panda`)
+    // await mongoose.connect(`${process.env.MONGODB_URI}kung_fu_panda`);
+    console.log('MongoDB connected successfully', client, query);
   } catch (error) {
     console.error('MongoDB connection error:', error);
     throw new Error(error)
