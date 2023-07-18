3e678360d3aacd08cafa3e942ac4fc0d8823ba10
devcode99 <ddev61032@gmail.com>
2023-07-18 15:27:20 +0530

    Quick Fix

--- src/db/connect.js
+++ src/db/connect.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 
 async function connectToMongoDB() {
   try {
-    const client = await MongoClient.connect(process.env.MONGODB_URI)
-    const query = client.db(`kung_fu_panda`)
+    // const client = await MongoClient.connect(process.env.MONGODB_URI)
+    // const query = client.db(`kung_fu_panda`)
     // await mongoose.connect(`${process.env.MONGODB_URI}kung_fu_panda`);
     console.log('MongoDB connected successfully', client, query);
   } catch (error) {
