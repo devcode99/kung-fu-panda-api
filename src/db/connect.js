b076e40bde7b81bbc428e7e3ddd164836ecb7818
rinkisingh8181 <rinkisingh8181@gmail.com>
2023-08-01 14:55:22 +0530

    Rmoeve the tmp package

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,4 +1,4 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 // const {MongoClient} = require('mongodb')
 require('dotenv').config();
 
@@ -6,7 +6,7 @@ async function connectToMongoDB(req, res) {
   try {
     // const client = await MongoClient.connect(process.env.MONGODB_URI)
     // const query = client.db(`kung_fu_panda`)
-    await require('../packages/mongoose').connect(`${process.env.MONGODB_URI}kung_fu_panda`);
+    await mongoose.connect(`${process.env.MONGODB_URI}kung_fu_panda`);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
